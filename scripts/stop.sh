8a788f17170bd79d619190f63faf086ec2b3864b
wisonic-s <51065359+wisonic-s@users.noreply.github.com>
2020-02-14 17:54:19 +0800

    修改端口

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo going to stop app
-ps aux | grep SimpleHTTPServer | grep 8080 | awk '{print "kill -9 " $2}' | bash
-processesNum=`ps aux | grep SimpleHTTPServer | grep 8080 | grep -v grep | wc -l | sed 's/ //g'`
+ps aux | grep SimpleHTTPServer | grep 8888 | awk '{print "kill -9 " $2}' | bash
+processesNum=`ps aux | grep SimpleHTTPServer | grep 8888 | grep -v grep | wc -l | sed 's/ //g'`
 if [ "$processesNum" == "0" ];then
     echo stopped app successfully!
     exit 0
